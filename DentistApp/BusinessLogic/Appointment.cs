49240299af06dacc0a9fd7a415345ca4a7946bca
Chaitanya Uttarwar <32759949+chaitanya046@users.noreply.github.com>
2019-08-05 20:52:05 -0400

    GUI Update and Appointment box population

--- DentistApp/BusinessLogic/Appointment.cs
+++ DentistApp/BusinessLogic/Appointment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DentistApp
 {
-    class Appointment
+    public class Appointment
     {
     }
 }
--- DentistApp/DentistApp/MainWindow.xaml.cs
+++ DentistApp/DentistApp/MainWindow.xaml.cs
@@ -22,7 +22,20 @@ namespace DentistApp
     {
         public MainWindow()
         {
+            String[] appointmentArray = new String[9];
             InitializeComponent();
+            //Populating Appointment Combobox
+            DateTime theTime = DateTime.Now;
+            DateTime initTime = new DateTime(theTime.Year, theTime.Month, theTime.Day, 9, 0, 0);
+            for (int i = 0; i < 8; i++)
+            {
+                appointmentArray[i] = initTime.ToString("HH:mm tt");
+                initTime = initTime.AddHours(1);
+            }
+            foreach (String apt in appointmentArray)
+            {
+                appointmentCombo.Items.Add(apt);
+            }
             //Populating Treatment Combobox
             string[] treatments = Enum.GetNames(typeof(TreatmentType));
             foreach (string name in treatments)
